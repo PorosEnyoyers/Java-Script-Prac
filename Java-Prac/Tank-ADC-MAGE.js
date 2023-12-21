cfc0854eb15c2d0a9a3185bae2b813cdf1f134a4
PorosEnyoyers <khoiphamleanh@gmail.com>
2023-12-20 21:21:40 -0800

    Finish play one round funtion.

--- Java-Prac/Tank-ADC-MAGE.js
+++ Java-Prac/Tank-ADC-MAGE.js
@@ -4,5 +4,30 @@ let arr = ["TANK", "ADC", "MAGE"];
 
 function getComputerChoice() {
     return (arr[(Math.floor(Math.random() * arr.length))])
-}
+};
 
+let playerChoice = prompt("Pick a class from ADC, MAGE and TANK", "ADC");
+
+let computerChoice = getComputerChoice();
+
+function roundOne(computerChoice, playerChoice) {
+    if (computerChoice == "MAGE" && playerChoice == "ADC") {
+        return ("Computer Win, Mage beats ADC")
+    } else if (computerChoice == "ADC" && playerChoice == "TANK") {
+        return ("Computer Win, ADC beats TANK")
+    } else if (computerChoice == "TANK" && playerChoice == "MAGE") {
+        return ("Computer Win, TANK beats MAGE")
+    } else if (computerChoice == "MAGE" && playerChoice == "TANK") {
+        return ("You Win, TANK beats MAGE")
+    } else if (computerChoice == "ADC" && playerChoice == "MAGE") {
+        return ("You Win, Mage beats ADC")
+    } else if (computerChoice == "TANK" && playerChoice == "ADC") {
+        return ("You Win, ADC beats TANK")
+    } else if (computerChoice == playerChoice) {
+        return ("You Draw.")
+    } else {
+        return ("Please Pick Between MAGE, TANK and ADC to play")
+    }
+};
+
+alert(roundOne(computerChoice, playerChoice));
